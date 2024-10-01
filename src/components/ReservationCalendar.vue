e3b3690d2cab5913ea8ecfbb607448e73998de6f
Guilherme Karam <gkaram@umov.me>
2024-10-01 01:18:50 -0300

    muda lib de calendario e ajusta margin body

--- src/components/ReservationCalendar.vue
+++ src/components/ReservationCalendar.vue
@@ -1,88 +1,45 @@
 <template>
-  <div>
-    <v-sheet
-      class="d-flex"
-      height="54"
-      tile
-    >
-      <v-select
-        v-model="type"
-        :items="types"
-        class="ma-2"
-        label="View Mode"
-        variant="outlined"
-        dense
-        hide-details
-      ></v-select>
-    </v-sheet>
-    <v-sheet>
-      <v-calendar
-        ref="calendar"
-        v-model="value"
-        :events="events"
-        :view-mode="type"
-        :weekdays="weekday"
-      ></v-calendar>
-    </v-sheet>
-  </div>
+    <Qalendar 
+      :events="events"
+      :config="config"
+    />
 </template>
 
 <script>
-  import { useDate } from 'vuetify'
+import { Qalendar } from "qalendar";
 
-  export default {
-    data: () => ({
-      type: 'month',
-      types: ['month', 'week', 'day'],
-      weekday: [0, 1, 2, 3, 4, 5, 6],
-      weekdays: [
-        { title: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
-        { title: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0] },
-        { title: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
-        { title: 'Mon, Wed, Fri', value: [1, 3, 5] },
-      ],
-      value: [new Date()],
-      events: [],
-      colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-      titles: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
-    }),
-    mounted () {
-      const adapter = useDate()
-      this.getEvents({ start: adapter.startOfDay(adapter.startOfMonth(new Date())), end: adapter.endOfDay(adapter.endOfMonth(new Date())) })
+export default {
+    components: {
+        Qalendar,
     },
-    methods: {
-      getEvents ({ start, end }) {
-        const events = []
 
-        const min = start
-        const max = end
-        const days = (max.getTime() - min.getTime()) / 86400000
-        const eventCount = this.rnd(days, days + 20)
-
-        for (let i = 0; i < eventCount; i++) {
-          const allDay = this.rnd(0, 3) === 0
-          const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-          const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-          const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-          const second = new Date(first.getTime() + secondTimestamp)
-
-          events.push({
-            title: this.titles[this.rnd(0, this.titles.length - 1)],
-            start: first,
-            end: second,
-            color: this.colors[this.rnd(0, this.colors.length - 1)],
-            allDay: !allDay,
-          })
+    data() {
+        return {
+            events: [
+                // ...
+                {
+                  title: "Advanced algebra",
+                  with: "Chandler Bing",
+                  time: { start: "2024-10-01 12:05", end: "2024-10-01 13:35" },
+                  color: "yellow",
+                  isEditable: true,
+                  id: "753944708f0f",
+                  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda corporis doloremque et expedita molestias necessitatibus quam quas temporibus veritatis. Deserunt excepturi illum nobis perferendis praesentium repudiandae saepe sapiente voluptatem!"
+                },
+                {
+                  title: "Ralph on holiday",
+                  with: "Rachel Greene",
+                  time: { start: "2022-05-10", end: "2022-05-22" },
+                  color: "green",
+                  isEditable: true,
+                  id: "5602b6f589fc"
+                }
+                // ...
+            ],
+            config: {
+              // see configuration section
+            }
         }
-
-        this.events = events
-      },
-      getEventColor (event) {
-        return event.color
-      },
-      rnd (a, b) {
-        return Math.floor((b - a + 1) * Math.random()) + a
-      },
     },
-  }
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -2,23 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as labsComponents from 'vuetify/labs/components'
-import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css'
-
-const vuetify = createVuetify({
-  components: {
-    ...labsComponents,
-    ...components
-  }
-})
+import "qalendar/dist/style.css";
 
 const pinia = createPinia()
 const app = createApp(App)
 
-app.use(vuetify)
 app.use(pinia)
 app.use(router)
 
