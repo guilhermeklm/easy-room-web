fb8af4411b44a984a79bbcdabadab51fcc827c39
Guilherme Karam <gkaram@umov.me>
2024-10-04 20:14:28 -0300

    wip reserva sala

--- src/components/ReserveRoom.vue
+++ src/components/ReserveRoom.vue
@@ -1,6 +1,37 @@
 <template>
-  <h2> Reservar sala </h2>
+  <h2>Reservar sala</h2>
+
+  <div>
+    <label>Titulo</label>
+    <input type="text" />
+    <label>Data e hora Inicio</label>
+    <VueDatePicker v-model="minDate" :format="format" :minDate="minDate" :timezone="timezone" />
+    <label>Data e hora Fim</label>
+    <VueDatePicker v-model="maxDate" :start-date="dateSuggestion" :format="format" :timezone="timezone" />
+    <label>Descricao</label>
+    <input type="text" maxlength="300" />
+  </div>
 </template>
 
-<script setup>
+<script>
+import VueDatePicker from '@vuepic/vue-datepicker'
+import moment from 'moment'
+const todayDate = moment().format('MM-DD-yyyy, HH:mm')
+const timePlusOneHour = moment().add(1, 'hour').format('MM-DD-yyyy, HH:mm')
+
+export default {
+  components: {
+    VueDatePicker
+  },
+  data() {
+    return {
+      minDate: todayDate,
+      dateSuggestion: timePlusOneHour,
+      maxDate: todayDate,
+      timezone: 'America/Sao_Paulo',
+      format: 'dd/MM/yyyy HH:mm'
+    }
+  },
+  methods: {}
+}
 </script>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import "qalendar/dist/style.css";
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+import moment from 'moment-timezone'
+moment.tz.setDefault('America/Sao_Paulo')
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(pinia)
 app.use(router)
+app.component(VueDatePicker)
 
 app.mount('#app')
